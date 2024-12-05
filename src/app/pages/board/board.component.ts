ff088eeec2d7ed1948791929248104100f137917
oloza <olozra@gmail.com>
2024-12-04 20:18:46 -0400

    drag and drop finished

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop,moveItemInArray } from '@angular/cdk/drag-drop';
+import { ToDo } from '../../models/todo.model';
 
 @Component({
   selector: 'app-board',
-  templateUrl: './board.component.html'
+  templateUrl: './board.component.html',
+  styles:[
+    `
+    /* Animate items as they're being sorted. */
+    .cdk-drop-list-dragging .cdk-drag {
+      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+    }
+
+    /* Animate an item that has been dropped. */
+    .cdk-drag-animating {
+      transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
+    }
+    `
+  ]
 })
 export class BoardComponent {
 
-  drop($event:CdkDragDrop<any[]>){
+  todos:ToDo[] = [
+    {
+      id:'1',
+      title:'todo 1'
+    },
+    {
+      id:'2',
+      title:'todo 2'
+    },
+    {
+      id:'3',
+      title:'todo 3'
+    },
+  ]
+
+  drop(event:CdkDragDrop<any[]>){
   // drop($event:any){
-    console.log($event);
+    // console.log($event);
+    moveItemInArray(this.todos,event.previousIndex,event.currentIndex);
   }
 }
